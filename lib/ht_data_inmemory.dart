c87cfca33a021eddecb83da3d12a61a019079b22
fulleni <fulleni@hotmail.com>
2025-04-26 08:30:41 +0100

    fix(ht_data_inmemory): update dependencies and improve package description
    - Remove uuid dependency as it's not needed
    - Add ht_http_client dependency for exceptions
    - Enhance package description in pubspec.yaml
    - Update library documentation in ht_data_inmemory.dart

--- lib/ht_data_inmemory.dart
+++ lib/ht_data_inmemory.dart
@@ -1,4 +1,5 @@
-/// A Very Good Project created by Very Good CLI.
+/// An in-memory implementation of the HtDataClient
+/// interface for testing or local development.
 library;
 
 export 'src/ht_data_inmemory.dart';
